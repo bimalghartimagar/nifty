e73cdf79023d98ad493bc54c3c39fad9a6433618
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2019-01-24 02:37:08 +0545

    using database uri from config

--- src/dao/base.py
+++ src/dao/base.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
+from utils.config import get_config
+
+DATABASE_URI = get_config().DATABASE_URI
 
 Base = declarative_base()
 
-engine = create_engine('postgresql://<USERNAME>:<PASSWORD>@<HOSTNAME>:<PORT>/<DB_NAME>')
+engine = create_engine(DATABASE_URI)
 
 Base.metadata.create_all(engine)
 

80cec2ddd21789af424c4cf2bdf3cd4576a8c965
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2019-01-24 17:10:43 +0545

    changing uppercase bank name to Upper Camel Case type

--- src/inserts.py
+++ src/inserts.py
@@ -11,7 +11,7 @@ def run_initial():
         {'bank_name': 'Standard Chartered Bank', 'bank_symbol':'SCBL'},
         {'bank_name': 'Sanima Bank Limited', 'bank_symbol':'SBL'},
         {'bank_name': 'Everest Bank Limited', 'bank_symbol':'EBL'},
-        {'bank_name': 'NEPAL SBI BANK LIMITED', 'bank_symbol':'SBI'},
+        {'bank_name': 'Nepal Sbi Bank Limited', 'bank_symbol':'SBI'},
         {'bank_name': 'Nepal Bank Limited', 'bank_symbol':'NBL'},
         {'bank_name': 'Lumbini Bikas Bank Ltd.', 'bank_symbol':'LBBL'},
         {'bank_name': 'Bank of Kathmandu Limited', 'bank_symbol':'BOKL'},
@@ -20,7 +20,7 @@ def run_initial():
         {'bank_name': 'Global IME Bank Ltd.', 'bank_symbol':'GIB'},
         {'bank_name': 'Machhapuchchhre Bank Limited', 'bank_symbol':'MBL'},
         {'bank_name': 'Agricultural Development Bank Limited', 'bank_symbol':'ADBL'},
-        {'bank_name': 'MEGA BANK NEPAL LIMITED', 'bank_symbol':'MB'}
+        {'bank_name': 'Mega Bank Nepal Limited', 'bank_symbol':'MB'}
     ]
 
     # Load banks data

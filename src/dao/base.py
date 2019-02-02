85f1b4561c2d2ea29be3cfe591f2e3570bfeea39
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2019-02-02 12:19:41 +0545

    removed database creation code

--- src/dao/base.py
+++ src/dao/base.py
@@ -8,7 +8,7 @@ DATABASE_URL = get_config().DATABASE_URL
 Base = declarative_base()
 
 engine = create_engine(DATABASE_URL)
-engine.execute("CREATE DATABASE IF NOT EXISTS {}".format(get_config().DB_NAME)) #create db
-engine = create_engine(DATABASE_URL+'/{}'.format(get_config().DB_NAME))
+# engine.execute("CREATE DATABASE IF NOT EXISTS {}".format(get_config().DB_NAME)) #create db
+# engine = create_engine(DATABASE_URL+'/{}'.format(get_config().DB_NAME))
 
 Session = sessionmaker(bind=engine)
